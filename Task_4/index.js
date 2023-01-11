e2ed8263a651fc670197fabcefcd2d15c3285635
Vitalii <duvit@users.noreply.github.com>
2023-01-11 23:30:23 +0200

    update

--- Task_4/index.js
+++ Task_4/index.js
@@ -1,30 +1,28 @@
-let time = 70;
-
-const timer = setInterval(() => {
-  const thisTime = time;
-  let seconds = 0;
-  let minutes = 0;
-
-  if (time > 3600) {
-    alert('You need enter time less 1 hour');
-    clearInterval(timer);
+const timer = (time) => {
+  if (time > 60 * 60) {
+    console.log("You need enter time less 1 hour");
+    return;
   }
-  else if (time < 60) {
-    minutes = "00";
-    seconds = time.toString().padStart(2, "0");
-  } else {
-    minutes = Math.floor(thisTime / 60)
+
+  const timerId = setInterval(() => {
+    const minutes = Math.floor(time / 60)
       .toString()
       .padStart(2, "0");
-    seconds = (thisTime % 60).toString().padStart(2, "0");
-  }
+    const seconds = Math.floor(time % 60)
+      .toString()
+      .padStart(2, "0");
 
-  console.log(`${minutes}:${seconds}`);
+    console.log(`${minutes}:${seconds}`);
 
-  if (time <= 1) {
-    clearInterval(timer);
-    console.log("Timer End");
-  }
-  time--;
-}, 1000);
+    if (time <= 1) {
+      clearInterval(timerId);
+      console.log("Timer End");
+    }
 
+    time--;
+  }, 1000);
+
+  return timerId;
+};
+
+timer(10);
